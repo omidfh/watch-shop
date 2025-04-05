246da25bc7adf3082334e1946e0fe37bae23f5e6
omid-faghihi <fhomid3@gmail.com>
2025-04-05 14:56:49 +0330

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Dosis } from "next/font/google";
 import Footer from "./_components/home/Footer";
 import AllContextProvider from "./_context/AllContextProvider";
 import { Suspense } from "react";
+import Loader from "./_components/loader/page";
 
 export const metadata = {
   title: "Home",
@@ -27,27 +28,27 @@ export default function RootLayout({
       <body
         className={`flex flex-col justify-between gap-12 ${dosis.className} h-[100dvh] `}
       >
-        <AllContextProvider>
-          {/* <div className="flex flex-col items-center w-full max-w-[65%]"> */}
-          {/* <div className="flex flex-col"> */}
-          <div className="flex w-[100%] justify-center">
-            <div
-              className={`flex items-center w-full max-w-[85%] lg:max-w-[65%] md:max-w-[75%] justify-start`}
-            >
-              <NavBar />
+        <Suspense fallback={<Loader />}>
+          <AllContextProvider>
+            {/* <div className="flex flex-col items-center w-full max-w-[65%]"> */}
+            {/* <div className="flex flex-col"> */}
+            <div className="flex w-[100%] justify-center">
+              <div
+                className={`flex items-center w-full max-w-[85%] lg:max-w-[65%] md:max-w-[75%] justify-start`}
+              >
+                <NavBar />
+              </div>
             </div>
-          </div>
-          <main>{children}</main>
-          {/* </div> */}
-          {/* //*FOOTER */}
-          <div className=" flex w-[100%] justify-center  bg-amber-100 bg-opacity-40 ">
-            <div className="flex items-center w-full max-w-[85%] lg:max-w-[65%] md:max-w-[75%]  justify-center">
-              <Suspense>
+            <main>{children}</main>
+            {/* </div> */}
+            {/* //*FOOTER */}
+            <div className=" flex w-[100%] justify-center  bg-amber-100 bg-opacity-40 ">
+              <div className="flex items-center w-full max-w-[85%] lg:max-w-[65%] md:max-w-[75%]  justify-center">
                 <Footer />
-              </Suspense>
+              </div>
             </div>
-          </div>
-        </AllContextProvider>
+          </AllContextProvider>
+        </Suspense>
         {/* </div> */}
       </body>
     </html>
